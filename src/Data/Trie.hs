76cfbea825a45e3ed32e3c64c21bc5c924fac8c6
zLeoAlex <z.alex.leo@outlook.com>
2022-05-01 16:38:28 +0800

    feat: Trie use map & impl toList

--- src/Data/Trie.hs
+++ src/Data/Trie.hs
@@ -1,36 +1,51 @@
+{-# LANGUAGE DeriveGeneric #-}
 module Data.Trie where
 
-import qualified Data.HashMap.Lazy as HM
-import qualified Data.Hashable     as H
-import qualified Data.Maybe        as M
-import           Prelude           as P hiding (elem)
+import           Control.DeepSeq (NFData)
+import qualified Data.Map        as M
+import qualified Data.Maybe      as M
+import           GHC.Generics    (Generic)
+import           Prelude         as P hiding (elem)
 
 -- | a trie tree, like an compressed strings by its prefix.
 data Trie tok
   = Trie
       { ends     :: Bool
-      , subTries :: HM.HashMap tok (Trie tok)
+      , subTries :: M.Map tok (Trie tok)
       }
-  deriving (Show)
+  deriving (Eq, Generic, Show)
+instance (NFData tok) => NFData (Trie tok)
 
 -- | /O(1)/, empty is a null TrieTree
 empty :: Trie tok
 empty = Trie {
   ends=False,
-  subTries=HM.empty
+  subTries=M.empty
 }
 
--- | /O(|toks|)/, insert a string into Trie tree
-insert :: (H.Hashable tok,Eq tok) => []tok -> Trie tok -> Trie tok
+-- | /O(|toks| * log(t))/, insert a string into Trie tree
+insert :: (Eq tok,Ord tok) => []tok -> Trie tok -> Trie tok
 insert [] tree     = tree {ends=True}
 insert (t:ts) tree@Trie{subTries=s} = tree {
-  subTries=HM.alter (Just. insert ts.M.fromMaybe empty) t s
+  subTries=M.alter (Just. insert ts.M.fromMaybe empty) t s
 }
 
--- | /O(|toks|)/, finds if 'toks' exist in trie tree
+-- | /O(|toks| * log(t))/, finds if 'toks' exist in trie tree
+--
 -- >>> "123" `elem` insert "123" empty
-elem :: (H.Hashable tok,Eq tok) => []tok -> Trie tok -> Bool
+-- True
+elem :: (Eq tok,Ord tok) => []tok -> Trie tok -> Bool
 elem [] tr = ends tr
-elem (t:ts) Trie{subTries=s} = case HM.lookup t s of
-    Nothing      -> False
-    Just subTree -> ts `elem` subTree
+elem (t:ts) Trie{subTries=s} = case M.lookup t s of
+  Nothing      -> False
+  Just subTree -> ts `elem` subTree
+
+-- | toList extract all token from Trie, in lexicographical order
+--
+-- >>> toList.insert "123".insert "124" $ empty
+-- ["123","124"]
+toList :: (Eq tok,Ord tok) => Trie tok -> [[tok]]
+toList Trie{ends=e,subTries=s}
+  | e = []:deeper
+  | otherwise = deeper
+  where deeper = M.foldlWithKey' (\l k v -> l <> ((k:) <$> toList v)) [] s
