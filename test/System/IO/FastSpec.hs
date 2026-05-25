e9e3797aaad2181fadec3ba558ec09215604dd12
zLeoAlex <z.alex.leo@outlook.com>
2026-05-26 00:38:59 +0800

    feat: optimize fast io

--- test/System/IO/FastSpec.hs
+++ test/System/IO/FastSpec.hs
@@ -2,34 +2,65 @@
 
 module System.IO.FastSpec where
 
-import           Control.Monad.ST      (runST)
-import           Data.Word             (Word)
-import qualified System.IO.Fast        as Fast
-import qualified System.IO.Fast.ST     as FastST
-import qualified System.IO.Fast.Stream as FastStream
-import           Test.Hspec            (Spec, describe, it, shouldBe)
+import           Control.Exception (bracket)
+import           Control.Monad.ST  (runST)
+import qualified Data.ByteString   as BS
+import           Data.Word         (Word)
+import qualified System.IO.Fast    as Fast
+import qualified System.IO.Fast.ST as FastST
+import           System.Directory  (getTemporaryDirectory, removeFile)
+import           System.IO         (Handle, SeekMode (AbsoluteSeek), hClose,
+                                    hFlush, hSeek, hSetBinaryMode, openTempFile)
+import           Test.Hspec        (Spec, describe, it, shouldBe)
 
 spec :: Spec
 spec = describe "System.IO.Fast" $ do
   it "parses common Online Judge tokens from a ByteString scanner" $ do
-    let scanner0 = Fast.scannerFromByteString "  -12 34\nword Z"
-        (a, scanner1) = Fast.nextInt scanner0
-        (b, scanner2) = Fast.nextWord scanner1
-        (token, scanner3) = Fast.nextByteString scanner2
-        (charValue, scanner4) = Fast.nextChar scanner3
+    scanner <- Fast.scannerFromByteString "  -12 34\nword Z"
+    a <- Fast.nextInt scanner
+    b <- Fast.nextWord scanner
+    token <- Fast.nextByteString scanner
+    charValue <- Fast.nextChar scanner
+    end <- Fast.maybeNextInt scanner
 
     a `shouldBe` (-12)
     b `shouldBe` 34
     token `shouldBe` "word"
     charValue `shouldBe` 'Z'
-    Fast.maybeNextInt scanner4 `shouldBe` Nothing
+    end `shouldBe` Nothing
 
   it "parses a fixed number of Int values" $ do
-    let (values, scanner) =
-          Fast.nextIntList 4 (Fast.scannerFromByteString "1 2 -3 4 rest")
+    scanner <- Fast.scannerFromByteString "1 2 -3 4 rest"
+    values <- Fast.nextIntList 4 scanner
+    token <- Fast.nextByteString scanner
 
     values `shouldBe` [1, 2, -3, 4]
-    fst (Fast.nextByteString scanner) `shouldBe` "rest"
+    token `shouldBe` "rest"
+
+  it "keeps the mutable scanner reusable without returning state" $ do
+    scanner <- Fast.scannerFromByteString "1  2"
+    first <- Fast.nextInt scanner
+    second <- Fast.nextInt scanner
+
+    first `shouldBe` 1
+    second `shouldBe` 2
+
+  it "streams handle input across internal Fast chunks" $ do
+    let chunkSize = 32 * 1024
+        longToken = BS.replicate (chunkSize + 3) 120
+        input = BS.replicate (chunkSize - 1) 32 <> "123 " <> longToken <> " Z"
+
+    withTempInput input $ \handle -> do
+      scanner <- Fast.scannerFromHandle handle
+      value <- Fast.nextInt scanner
+      token <- Fast.nextByteString scanner
+      charValue <- Fast.nextChar scanner
+      end <- Fast.maybeNextInt scanner
+
+      value `shouldBe` 123
+      token `shouldBe` longToken
+      charValue `shouldBe` 'Z'
+      end `shouldBe` Nothing
 
   it "parses common tokens with an ST scanner" $ do
     let result = runST $ do
@@ -43,24 +74,20 @@ spec = describe "System.IO.Fast" $ do
 
     result `shouldBe` (-12, 34 :: Word, "word", 'Z', Nothing)
 
-  it "streams tokens across chunk boundaries" $ do
-    scanner <- FastStream.streamScannerFromChunks ["  -", "12 3", "4\nwo", "rd Z"]
-    a <- FastStream.nextIntIO scanner
-    b <- FastStream.nextWordIO scanner
-    token <- FastStream.nextByteStringIO scanner
-    charValue <- FastStream.nextCharIO scanner
-    end <- FastStream.maybeNextIntIO scanner
+withTempInput :: BS.ByteString -> (Handle -> IO a) -> IO a
+withTempInput input action =
+  bracket create cleanup $ \(_, handle) -> do
+    BS.hPut handle input
+    hFlush handle
+    hSeek handle AbsoluteSeek 0
+    action handle
+  where
+    create = do
+      dir <- getTemporaryDirectory
+      result@(_, handle) <- openTempFile dir "fastio-spec.input"
+      hSetBinaryMode handle True
+      pure result
 
-    a `shouldBe` (-12)
-    b `shouldBe` (34 :: Word)
-    token `shouldBe` "word"
-    charValue `shouldBe` 'Z'
-    end `shouldBe` Nothing
-
-  it "streams a fixed number of Int values" $ do
-    scanner <- FastStream.streamScannerFromChunks ["1 ", "2 -", "3 4 rest"]
-    values <- FastStream.nextIntListIO 4 scanner
-    token <- FastStream.nextByteStringIO scanner
-
-    values `shouldBe` [1, 2, -3, 4]
-    token `shouldBe` "rest"
+    cleanup (path, handle) = do
+      hClose handle
+      removeFile path
